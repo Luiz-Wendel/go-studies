de76c777445b4781bdaca58f1232826caacd6e23
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-03-21 22:34:10 -0300

    loop - nesting loops (adding comments)

--- 03-control-flow/02-loop-nesting-loops/main.go
+++ 03-control-flow/02-loop-nesting-loops/main.go
@@ -6,6 +6,7 @@ func main() {
 	for firstIndex := 0; firstIndex <= 10; firstIndex++ {
 		fmt.Printf("Outer loop: %d\n", firstIndex)
 
+		// we can add as many nesting loops we want
 		for secondIndex := 0; secondIndex < 3; secondIndex++ {
 			fmt.Printf("\t\t Inner loop: %d\n", secondIndex)
 		}
