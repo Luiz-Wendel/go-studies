ed95e653eda56ddb2a624a1345e29698a215dd50
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-04-13 15:23:40 -0300

    fix comma ok idiom example code

--- 10-channels/06-comma-ok-idiom/main.go
+++ 10-channels/06-comma-ok-idiom/main.go
@@ -3,15 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	even := make(chan int)
-	odd := make(chan int)
-	quit := make(chan bool)
+	/*
+		even := make(chan int)
+		odd := make(chan int)
+		// another way
+		quit := make(chan bool)
 
-	go send(even, odd, quit)
+		go send(even, odd, quit)
 
-	receive(even, odd, quit)
+		receive(even, odd, quit)
+	*/
+
+	c := make(chan int)
+
+	go func() {
+		c <- 42
+
+		close(c)
+	}()
+
+	// get the `value` stored in the channel and a truthy boolean indicating it has values
+	value, ok := <-c
+
+	fmt.Println(value, ok)
+
+	// as the channel has no more values, it will return the `zero value` and a falsy boolean
+	value, ok = <-c
+
+	fmt.Println(value, ok)
 }
 
+/*
 func send(even, odd chan<- int, quit chan<- bool) {
 	for index := 0; index < 100; index++ {
 		if index%2 == 0 {
@@ -21,6 +43,7 @@ func send(even, odd chan<- int, quit chan<- bool) {
 		}
 	}
 
+	// closing a channel
 	close(quit)
 }
 
@@ -37,3 +60,4 @@ func receive(even, odd <-chan int, quit <-chan bool) {
 		}
 	}
 }
+*/
