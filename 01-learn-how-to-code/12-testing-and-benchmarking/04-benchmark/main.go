18720baafb0dbeea01941ad003a993a56ea88da2
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-04-19 16:11:41 -0300

    benchmark

---
+++ 01-learn-how-to-code/12-testing-and-benchmarking/04-benchmark/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Luiz-Wendel/go-studies/01-learn-how-to-code/12-testing-and-benchmarking/04-benchmark/saying"
+)
+
+func main() {
+	fmt.Println(saying.Greet("James"))
+}
