c149667004caf98f7a92e3c60a6e0b9551c5f448
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-05-20 11:26:36 -0300

    add exercises info

--- 03-it-bootcamp-meli/01-go-bases/01/exercises/morning/01/main.go
+++ 03-it-bootcamp-meli/01-go-bases/01/exercises/morning/01/main.go
@@ -1,3 +1,9 @@
+/*
+	Exercício 1 - Imprimindo o nome na tela
+		1. Crie uma aplicação que tenha uma variável “nome” e outra “idade”.
+		2. Imprima no terminal o valor de cada variável.
+*/
+
 package main
 
 import "fmt"
