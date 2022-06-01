b945716f059d01415754276110ac7767bb185c01
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-01 11:16:57 -0300

    add morning exercise 03

---
+++ 03-it-bootcamp-meli/02-go-web/01/exercises/morning/03/main.go
@@ -0,0 +1,62 @@
+/*
+	Exercício 3 - Listar Entidade
+
+		Já tendo criado e testado nossa API que nos recebe, geramos uma rota que retorna uma lista do tema escolhido.
+			1. Dentro do "main.go", crie uma estrutura de acordo com o tema com os campos correspondentes.
+			2. Crie um endpoint cujo caminho é /thematic (plural). Exemplo: “/products”
+			3. Crie uma handler para o endpoint chamada "GetAll".
+			4. Crie um slice da estrutura e retorne-a por meio de nosso endpoint.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	Id, FirstName, LastName, Email, CreationDate string
+	Age, Height                                  uint
+	Active                                       bool
+}
+
+func getFileContent(file string) []byte {
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal("error when opening file: ", err)
+	}
+
+	return content
+}
+
+func serializeJson(content []byte, variable interface{}) {
+	err := json.Unmarshal(content, variable)
+	if err != nil {
+		log.Fatal("error while reading json: ", err)
+	}
+}
+
+func GetAll(c *gin.Context) {
+	content := getFileContent("./03-it-bootcamp-meli/02-go-web/01/exercises/morning/01/users.json")
+
+	var payload []user
+
+	serializeJson(content, &payload)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": payload,
+	})
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/users", GetAll)
+
+	router.Run()
+}
