629cd89281cb4dec09ad556857cda20f1d47e7a1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-06-06 09:19:32 -0300

    add morning exercise 01

---
+++ 03-it-bootcamp-meli/02-go-web/03/exercises/morning/01/internal/users/service.go
@@ -0,0 +1,48 @@
+package users
+
+import "github.com/google/uuid"
+
+type Service interface {
+	GetAll() ([]User, error)
+	Store(firstName, lastName, email, creationDate string, age, height uint, active bool) (User, error)
+	Update(id, firstName, lastName, email, creationDate string, age, height uint, active bool) (User, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func (s service) GetAll() ([]User, error) {
+	users, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s service) Store(firstName, lastName, email, creationDate string, age, height uint, active bool) (User, error) {
+	id := uuid.NewString()
+
+	user, err := s.repository.Store(id, firstName, lastName, email, creationDate, age, height, active)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (s service) Update(id, firstName, lastName, email, creationDate string, age, height uint, active bool) (User, error) {
+	user, err := s.repository.Update(id, firstName, lastName, email, creationDate, age, height, active)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
