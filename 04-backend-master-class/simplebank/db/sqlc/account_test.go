943e3cee084784e096b4e4bb4b68e480582bfb6b
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-06-13 17:14:11 -0300

    add random values to account parameters

--- 04-backend-master-class/simplebank/db/sqlc/account_test.go
+++ 04-backend-master-class/simplebank/db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/Luiz-Wendel/go-studies/04-backend-master-class/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "John Doe",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
