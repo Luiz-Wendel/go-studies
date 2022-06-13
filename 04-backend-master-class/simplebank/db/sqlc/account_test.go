50491b7b6ca460340f21d4a4f59db1541cbcbbe2
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-06-13 17:26:16 -0300

    add get account test

--- 04-backend-master-class/simplebank/db/sqlc/account_test.go
+++ 04-backend-master-class/simplebank/db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/Luiz-Wendel/go-studies/04-backend-master-class/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -26,4 +27,23 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+	recoveredAccount, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, recoveredAccount)
+
+	require.Equal(t, createdAccount.ID, recoveredAccount.ID)
+	require.Equal(t, createdAccount.Owner, recoveredAccount.Owner)
+	require.Equal(t, createdAccount.Balance, recoveredAccount.Balance)
+	require.Equal(t, createdAccount.Currency, recoveredAccount.Currency)
+	require.WithinDuration(t, createdAccount.CreatedAt, recoveredAccount.CreatedAt, time.Second)
 }
