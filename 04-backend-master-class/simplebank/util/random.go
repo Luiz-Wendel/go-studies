a84c5159ee2937f66f204d883ea794a7d42f16c9
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-06-13 16:57:27 -0300

    add RandomString generator to util package

--- 04-backend-master-class/simplebank/util/random.go
+++ 04-backend-master-class/simplebank/util/random.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 // `func init`is a special function that will be called automatically when the package is first used
 
@@ -17,4 +20,17 @@ func RandomInt(min, max int64) int64 {
 	// `rand.Int63n` will return an random integer between `0` and `max-min`
 	// by adding `min` a random integer between `min` and `max` is generated
 	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length `size`
+func RandomString(size int) string {
+	var stringBuilder strings.Builder
+	alphabetLength := len(alphabet)
+
+	for index := 0; index < size; index++ {
+		char := alphabet[rand.Intn(alphabetLength)]
+		stringBuilder.WriteByte(char)
+	}
+
+	return stringBuilder.String()
 }
