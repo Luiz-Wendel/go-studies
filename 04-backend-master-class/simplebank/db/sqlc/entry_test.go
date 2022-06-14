77356753a8df573ae94ba57bf7a80708ec42cbf0
Luiz Wendel <luiz.balbino@mercadolivre.com>
2022-06-14 15:04:45 -0300

    add get entry test

--- 04-backend-master-class/simplebank/db/sqlc/entry_test.go
+++ 04-backend-master-class/simplebank/db/sqlc/entry_test.go
@@ -3,16 +3,15 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/Luiz-Wendel/go-studies/04-backend-master-class/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateEntry(t *testing.T) {
-	createdAccount := CreateRandomAccount(t)
-
+func createRandomEntry(t *testing.T, id int64) Entry {
 	arg := CreateEntryParams{
-		AccountID: createdAccount.ID,
+		AccountID: id,
 		Amount:    util.RandomMoney(),
 	}
 
@@ -25,4 +24,27 @@ func TestCreateEntry(t *testing.T) {
 
 	require.NotZero(t, createdEntry.ID)
 	require.NotZero(t, createdEntry.CreatedAt)
+
+	return createdEntry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createdAccount := CreateRandomAccount(t)
+
+	createRandomEntry(t, createdAccount.ID)
+}
+
+func TestGetEntry(t *testing.T) {
+	createdAccount := CreateRandomAccount(t)
+
+	createdEntry := createRandomEntry(t, createdAccount.ID)
+
+	recoveredEntry, err := testQueries.GetEntry(context.Background(), createdEntry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, recoveredEntry)
+
+	require.Equal(t, createdEntry.ID, recoveredEntry.ID)
+	require.Equal(t, createdEntry.AccountID, recoveredEntry.AccountID)
+	require.Equal(t, createdEntry.Amount, recoveredEntry.Amount)
+	require.WithinDuration(t, createdEntry.CreatedAt, recoveredEntry.CreatedAt, time.Second)
 }
